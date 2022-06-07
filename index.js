d46099a2984a46d8eecbc64bb6dadfaddbe61dac
ropi59 <mrseed@hotmail.com>
2022-06-07 15:19:28 +0200

    index.js maPremiereErreur = 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let monPremierCommit = 1;
 let maPremiereBranche = 0;
 let maPremiereFusion = 0;
-let maPremiereErreur = 0;
+let maPremiereErreur = 1;
 let jeVaisMalNommerMonProchainCommit = 1;
 let unicorneMode = 1;
